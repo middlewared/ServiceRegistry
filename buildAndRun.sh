af8cff85f5f1f218803469fe5317f24ecb90d704
middlewared <github@middlewared.com>
2018-02-12 12:30:49 +0100

    updated gitignore, cleanup of docker

--- buildAndRun.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mvn clean package && docker build -t com.baku/PersistentServiceRegistry .
-docker rm -f PersistentServiceRegistry || true && docker run -d -p 8080:8080 -p 4848:4848 --name PersistentServiceRegistry com.baku/PersistentServiceRegistry 
