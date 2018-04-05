b1184179a0b705135a82cb9be218a126398786ef
middlewared <github@middlewared.com>
2018-04-05 17:38:21 +0200

    changes startup script to remove previously build container and stop image

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
+mvn clean package
+docker stop PersistentServiceRegistry
+docker rm PersistentServiceRegistry
 docker build -t persistentserviceregistry:1.0 .
 docker run -d -p 8080:8080 --name PersistentServiceRegistry persistentserviceregistry:1.0
 
